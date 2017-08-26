032c0814465e7d3797bc1cfdbc3334f116d82c07
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2017-08-25 21:40:44 -0400

    Complete implementation of watchdog_timer and provide example

---
+++ examples/watchdog_timer.cxx
@@ -0,0 +1,49 @@
+/* -*- coding: utf-8; mode: c++; tab-width: 3; indent-tabs-mode: nil -*-
+
+Copyright 2017 Raffaello D. Di Napoli
+
+This file is distributed under the terms of the Creative Commons Attribution-ShareAlike 4.0 International
+(CC BY-SA 4.0) license.
+------------------------------------------------------------------------------------------------------------*/
+
+/*! @file
+Watchdog timer example
+
+This program lets the user turn on or off an LED (on B4, active high) by grounding D4 or D5 (active low) while
+the watchdog timer is running. After 4 seconds of inactivity, the microcontroller is reset. */
+
+/* MCU ATtiny4313 */
+#define F_CPU 1000000
+
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <rawr/binary_input_pin.hxx>
+#include <rawr/hw/binary_output_pin.hxx>
+#include <rawr/hw/watchdog_timer.hxx>
+#include <rawr/startup.hxx>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void uc_main() {
+   typedef rawr::hw::binary_output_pin<'B', 4, false /*initialize to logic 0*/> led;
+   typedef rawr::binary_input_pin<'D', 4, true /*initialize with pull-up*/> button_d4;
+   typedef rawr::binary_input_pin<'D', 5, true /*initialize with pull-up*/> button_d5;
+   typedef rawr::hw::watchdog_timer wdt;
+
+   // Reset after 4 seconds of inactivity.
+   wdt::enable(wdt::timeout::_4s);
+
+   button_d4::set_callback([] (bool) {
+      led::set();
+      wdt::reset();
+   });
+   button_d5::set_callback([] (bool) {
+      led::clear();
+      wdt::reset();
+   });
+
+   sei();
+   for (;;) {
+      sleep_cpu();
+   }
+}
--- include/rawr/hw/watchdog_timer.hxx
+++ include/rawr/hw/watchdog_timer.hxx
@@ -14,16 +14,41 @@ more details.
 
 #pragma once
 
+#include <avr/wdt.h>
+#include <rawr/hw/consistent_names.hxx>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 namespace rawr { namespace hw {
 
-class watchdog {
-   static constexpr void arm() {
+class watchdog_timer {
+public:
+   enum class timeout {
+      _8s    = 0b1001,
+      max    = _8s,
+      _4s    = 0b1000,
+      _2s    = 0b0111,
+      _1s    = 0b0110,
+      _500ms = 0b0101,
+      _250ms = 0b0100,
+      _125ms = 0b0011,
+      _64ms  = 0b0010,
+      _32ms  = 0b0001,
+      _16ms  = 0b0000,
+      min    = _16ms
+   };
+
+public:
+   static void disable() {
+      wdt_disable();
    }
 
-   static constexpr void disarm() {
+   static void enable(enum timeout timeout) {
+      wdt_enable(static_cast<uint8_t>(timeout));
    }
 
-   static constexpr void reset() {
+   static void reset() {
+      wdt_reset();
    }
 };
 
